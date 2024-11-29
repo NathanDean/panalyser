e085e82402566808e0c3cfb6bdb9d92cebfb7c8b
Nathan Dean <nathandean@hotmail.co.uk>
2024-11-29 02:11:04 +0000

    Add __init__.py, pyproject.toml, LICENSE, and requirements.txt

---
+++ src/__init__.py
@@ -0,0 +1,13 @@
+__version__ = "0.1.0"
+__author__ = "Nathan Dean"
+
+from .panalyser import (
+
+    run_imputation,
+    optimise_imputation,
+    run_fe_regression,
+    run_re_regression,
+    combine_regression_results,
+    run_combined_regression
+
+)
